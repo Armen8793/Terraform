422b0e700a969a39247a59f9befe6691c64b36b2
Armen Petrosyan <petrosyan1987@inbox.ru>
2021-12-29 11:44:03 +0400

    High Available Website ZeroDowntime with Green/Blue Deployment

---
+++ user_d.sh
@@ -0,0 +1,19 @@
+yum -y update
+yum -y install httpd
+
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+
+cat <<EOF > /var/www/html/index.html
+<html>
+<body bgcolor="black">
+<h2><font color="gold">Build by Armen via Terraform <font color="red"> v0.12</font><h2/><br>
+<font color="green">Server PrivateIP: <font color="aqua">$myip<br><br>
+
+<font color="magenta">
+<b>Version 1.0</b>
+</body>
+</html>
+EOF
+
+sudo service httpd start
+chkconfig httpd on
